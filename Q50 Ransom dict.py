dc59e6288bb75635d0735de9cc09d00ffa7c55f3
manan-malhotra <60249740+manan-malhotra@users.noreply.github.com>
2020-06-10 16:21:23 +0530

    Hackerrank

---
+++ Q50 Ransom dict.py
@@ -0,0 +1,22 @@
+n, m = map(int, input().split())
+s1 = input().split(" ")
+s2 = input().split(" ")
+s = {}
+for i in s1:
+    if i in s:
+        s[i] += 1
+    else:
+        s[i] = 1
+same = 0
+for i in s2:
+    if i in s:
+        if s[i] > 0:
+            s[i] -= 1
+        else:
+            same += 1
+    else:
+        same += 1
+if same == 0:
+    print("Yes")
+else:
+    print("No")
---
+++ Q54 MinABSdiff.py
@@ -0,0 +1,13 @@
+def minimumAbsoluteDifference(arr):
+    arr.sort()
+    min_dif = abs(arr[0] - arr[1])
+    for i in range(len(arr)-1):
+        dif = abs(arr[i] - arr[i+1])
+        if dif < min_dif:
+            min_dif = dif
+    return min_dif
+
+
+n = int(input())
+arr = list(map(int, input().rstrip().split()))
+print(minimumAbsoluteDifference(arr))
